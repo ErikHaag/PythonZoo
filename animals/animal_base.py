67cc37647ebd9e261640629dc7034b09c5a75787
Calculus_is_fun at school! <60990391+ErikHaag@users.noreply.github.com>
2025-04-22 13:14:56 -0500

    Flushed out the basic animal template

--- animals/animal_base.py
+++ animals/animal_base.py
@@ -3,8 +3,17 @@ class animal:
         # look at that animal!
         self.happiness = 100
         self.hunger = 100
+        self.activity_timer = 0
     
     def step(self):
         # update animal's stats every minute
         self.happiness -= 1
         self.hunger -= 1
+        self.activity_timer -= 1
+        if self.activity_timer <= 0:
+            # only start activity when not already doing one
+            self.maybe_do_activity()
+    
+    def maybe_do_activity(self):
+        # do action, with certain actions being more important. 
+        pass
