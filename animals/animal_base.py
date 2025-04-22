b15dce2b14b012b08160dca1562f93917febba18
Calculus_is_fun at school! <60990391+ErikHaag@users.noreply.github.com>
2025-04-22 12:40:38 -0500

    added temple function for animal class

--- animals/animal_base.py
+++ animals/animal_base.py
@@ -1,2 +1,10 @@
 class animal:
-    pass
+    def __init__(self):
+        # look at that animal!
+        self.happiness = 100
+        self.hunger = 100
+    
+    def step(self):
+        # update animal's stats every minute
+        self.happiness -= 1
+        self.hunger -= 1
